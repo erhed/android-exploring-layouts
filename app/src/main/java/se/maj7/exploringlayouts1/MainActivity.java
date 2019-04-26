727c80db993ad2c7c45eeb8c842cc6dfef01f318
erhed <erik.hedrenius@gmail.com>
2019-04-26 15:25:26 +0200

    three views

--- app/src/main/java/se/maj7/exploringlayouts1/MainActivity.java
+++ app/src/main/java/se/maj7/exploringlayouts1/MainActivity.java
@@ -10,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+    }
+
+    void loadMenuLayout(View v) {
+        setContentView(R.layout.main_menu);
     }
 
     void loadConstraintLayout(View v) {
