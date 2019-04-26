de02b59afcb592945f83dc3eeec4ea797b123f55
erhed <erik.hedrenius@gmail.com>
2019-04-26 13:14:03 +0200

    init commit

---
+++ app/src/main/java/se/maj7/exploringlayouts1/MainActivity.java
@@ -0,0 +1,22 @@
+package se.maj7.exploringlayouts1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+
+    void loadConstraintLayout(View v) {
+        setContentView(R.layout.activity_main);
+    }
+
+    void loadTableLayout(View v) {
+        setContentView(R.layout.my_table_layout);
+    }
+}
